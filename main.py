2aa05811030afcd2203a93302f570c555c1f3c06
Pierre Barbé <barb.pierre@yahoo.fr>
2024-05-06 12:32:06 +0200

    City weather extraction - SSL fixing

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+# City name import
+city = input('Please input the city name : ')
+
+# Corresponding wttr.in url for selected city
+url = "https://wttr.in/{}".format(city)
+
+# Weather ASCII answer from wttr url request
+weather_res = requests.get(url)
+
+print(weather_res.text)
