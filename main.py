2c240a0111ab3c9ef952a054ec71867328734cd0
Pierre Barbé <barb.pierre@yahoo.fr>
2024-05-06 12:46:45 +0200

    Recipe Request feature - ok

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from openai import OpenAI
 
 # City name import
 city = input('Please input the city name : ')
@@ -10,3 +11,17 @@ url = 'https://wttr.in/{}'.format(city)
 weather_res = requests.get(url)
 
 print(weather_res.text)
+
+# OpenAI key for requests (warning : do not exceed subscription requests !)
+client = OpenAI(api_key="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")
+
+# 
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a cooking assistant, skilled in prociding detailed recipies based on local weather."},
+    {"role": "user", "content": "Provide a cooking recipe matching the following weather conditions and geographical coordinates." + weather_res.text}
+  ]
+)
+
+print(completion.choices[0].message)
