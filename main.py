49d7055f31b25faeec7ae0e0e1782b132d768da4
Pierre Barbé <barb.pierre@yahoo.fr>
2024-05-06 12:40:42 +0200

    Weather request - String and SSL dll missing fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 city = input('Please input the city name : ')
 
 # Corresponding wttr.in url for selected city
-url = "https://wttr.in/{}".format(city)
+url = 'https://wttr.in/{}'.format(city)
 
 # Weather ASCII answer from wttr url request
 weather_res = requests.get(url)
